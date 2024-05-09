c9e078606a74c686e9e16894d97efdea4352f6d6
Osas Ray <168213848+OsasRay@users.noreply.github.com>
2024-05-09 16:30:02 +0100

    Update software.py
    we just did addition of num1 and num2 and SumALL

--- software.py
+++ software.py
@@ -1,4 +1,8 @@
 import datetime
 today = datetime.datetime.now()
 if today.month == 4 and today.day == 27 and today.year == 2024:
+    num1 = 2
+    num2 = 4
+    sumAll = num1 + num2
+    print(sumAll)
     print("hurry is my birthday")
